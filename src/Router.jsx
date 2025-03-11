6fd427b2186ddced51ec38fde8200eb1a8de344f
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-11 19:10:05 +0100

    User page added (Router)

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import Homepage from "./pages/homepage/Homepage"
 import SignInPage from "./pages/signIn/SignIn"
+import UserPage from "./pages/user/User"
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Homepage />} />
                 <Route path="sign-in" element={<SignInPage />} />
+                <Route path="user" element={<UserPage />} />
             </Route>
         </Routes>
     </Router>
