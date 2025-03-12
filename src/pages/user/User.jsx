9df1bd111897a5e0b5c7ed7877253155037e7ed7
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-12 20:01:38 +0100

    First draft: User page

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,32 +1,65 @@
+import { Link } from "react-router"
 import "./user.css"
 
 function UserPage() {
   return (
-    <div className="main bg-dark">
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
-        <form>
-          <div className="input-wrapper">
-            <label for="username">Username</label
-            ><input type="text" id="username" />
+    <div className="user-container bg-dark">
+
+      <div className="user-name">
+        <h1>
+          Welcome back<br />
+          {/* Prénom et nom fetchés plus tard : */}
+          Tony Jarvis!
+        </h1>
+        <div className="buttons-container">
+          <button className="button">Save</button>
+          <button className="button">Cancel</button>
+        </div>
+      </div>
+
+      <h2 className="sr-only">Accounts</h2>
+
+      <div className="accounts-container">
+        
+        {/* Mettre le composant Card une fois les données fetchées */}
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
           </div>
-          <div className="input-wrapper">
-            <label for="password">Password</label
-            ><input type="password" id="password" />
+          <div className="account-content-wrapper cta">
+            {/* Exemple de bouton pour aller à la page "transaction" */}
+            <Link to="transaction" className="header-logo">
+              <button className="button">View transactions</button>
+            </Link>
           </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" /><label for="remember-me"
-              >Remember me</label
-            >
+        </section>
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
           </div>
-          {/* <!-- PLACEHOLDER DUE TO STATIC SITE -->
-          <a href="./user.html" className="sign-in-button">Sign In</a>
-          <!-- SHOULD BE THE BUTTON BELOW -->
-          <!-- <button className="sign-in-button">Sign In</button> -->
-          <!--  --> */}
-        </form>
-      </section>
+          <div className="account-content-wrapper cta">
+            <button className="button">View transactions</button>
+          </div>
+        </section>
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="button">View transactions</button>
+          </div>
+        </section>
+      </div>
+
     </div>
   )
 }
