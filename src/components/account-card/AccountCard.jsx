1429075a8628a12f7f57584b993bed4cad57b989
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-13 18:28:06 +0100

    Link added

--- src/components/account-card/AccountCard.jsx
+++ src/components/account-card/AccountCard.jsx
@@ -1,17 +1,18 @@
 import PropTypes from "prop-types"
 import "./accountCard.css"
+import { Link } from "react-router"
 
 function AccountCard({title, amount, description}) {
   return (
     <section className="account">
         <div className="account-content-wrapper">
-            <h3 className="account-title">{title}</h3>
-            <p className="account-amount">${amount}</p>
-            <p className="account-amount-description">{description}</p>
+          <h3 className="account-title">{title}</h3>
+          <p className="account-amount">${amount}</p>
+          <p className="account-amount-description">{description}</p>
         </div>
-        <div className="account-content-wrapper cta">
-            <button className="button">View transactions</button>
-        </div>
+        <Link to="transaction">
+          <button className="button">View transactions</button>
+        </Link>
     </section>
   )
 }
