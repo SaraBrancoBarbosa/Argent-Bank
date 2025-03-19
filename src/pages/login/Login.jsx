7e18cdd844f97d8b73db6d3a3fc4fc6769dae0ae
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-19 18:04:15 +0100

    Redux added (not working for now) + alert message CSS

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react"
 import "./login.css"
+import { useDispatch, useSelector } from "react-redux"
+import { login } from "../../redux/userSlice"
+import { useNavigate } from "react-router"
 
+/******
+Avec le "Remember me" coché, stocker le token localStorage ?
+Et quand il n'est pas coché, stocker le token dans sessionStorage ?
+Ou stocker le token dans le store ?
+******/
 function LoginPage() {
   
   // Avec useState, on stocke les valeurs des champs username, password et rememberMe
@@ -8,11 +16,26 @@ function LoginPage() {
   const [password, setPassword] = useState("")
   const [rememberMe, setRememberMe] = useState(false)
 
-  // On gère la soumission du formulaire avec le onSubmit
-  // On évite que la page se recharge avec le e.preventDefault
-  // Finir de remplir, avec l'API ?
-  const handleSubmit = (event) => {
+  // redux state
+  const {loading, error} = useSelector((state) => state.user)
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLoginEvent = (event) => {
     event.preventDefault()
+    let userCredentials={
+      username, password
+    }
+    dispatch(login(userCredentials)).then((result) => {
+      // si login est fulfilled (userSlice) alors on aura quelque chose à payload
+      // On va clear le formulaire et être redirigé vers la homepage
+      if(result.payload) {
+        setUsername("")
+        setPassword("")
+        navigate("/profile")
+      }
+    })
   }
 
   return (
@@ -22,7 +45,7 @@ function LoginPage() {
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleLoginEvent}>
 
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
@@ -59,8 +82,12 @@ function LoginPage() {
 
           {/* Le bouton du fichier HTML de base, changer par un lien ? */}
           <button type="submit" className="button">
-            Sign In
+            {loading ? "Loading..." : "Sign in"}
           </button>
+
+          {error && (
+            <div className="alert" role="alert">{error}</div>
+          )}
           
         </form>
 
