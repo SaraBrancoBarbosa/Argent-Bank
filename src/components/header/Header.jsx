c12618d8f85f14d253e9ad683b3a35bf5df050ac
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-21 15:53:10 +0100

    Update: state management (token) + better writing of the header switch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,63 +1,62 @@
 import { Link } from "react-router-dom"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getProfile } from "../../redux/profileSlice"
 import "./header.css"
-import { useState } from "react"
-
-const logo = "/assets/img/argentBankLogo.png"
 
 function Header() {
     
-    // L'état pour savoir si le user est connecté ou non.
-    // J'aimerais remplacer ça par un "vrai" état plus tard, avec un hook
-    const [isLoggedIn, setIsLoggedIn] = useState(true)
+    const dispatch = useDispatch()
+
+    // The state to know if the user is connected or not
+    const { token } = useSelector(state => state.token)
+    const { firstName } = useSelector(state => state.profile)
 
     const handleSignOut = (e) => {
         e.preventDefault()
         window.localStorage.clear()
-        window.location.href="/login"
-        setIsLoggedIn(false)
+        window.sessionStorage.clear()
+        window.location.href="/"
     }
 
-    const renderHeader = () => {
-        // Quand on est connecté en tant qu'utilisateur :
-        if (isLoggedIn === true) {
-            return (
-                <div className="sign-out">
-                    <Link to="/profile" className="header-item">
-                        <i className="fa fa-user-circle"></i>
-                        <p className="header-item-text">Tony</p>
-                    </Link>
-                    <Link to="/" onClick={handleSignOut} className="header-item">
-                        <i className="header-item-icon fa fa-sign-out"></i>
-                        <p className="header-item-text">Sign Out</p>
-                    </Link>
-                </div>
-            )
+    useEffect(() => {
+        if(token && firstName === null) {
+            // Async function to load the userprofile information from the back-end data (by using redux)
+            dispatch(getProfile(token))
         }
-
-        // Quand on n'est pas connecté :
-        return (
-            <div>
-                <Link to="/login" className="header-item">
-                    <i className="header-item-icon fa fa-user-circle"></i>
-                    <p className="header-item-text">Sign In</p>
-                </Link>
-            </div>
-        )
-    }
+    },[token, firstName, dispatch])
 
     return (
         <header className="header">
             <Link to="/" className="header-logo">
                 <img
                     className="header-logo-image"
-                    src={logo}
+                    src="/assets/img/argentBankLogo.png"
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
 
-            {/* On affiche ici le bon header (partie droite) en fonction de l'état de connexion */}
-            {renderHeader()}
+            {/* To display the correct header depending on the connexion state */}
+            {token ? (
+                <div className="sign-out">
+                    <Link to="/profile" className="header-item">
+                        <i className="fa fa-user-circle"></i>
+                        <p className="header-item-text">{firstName}</p>
+                    </Link>
+                    <Link to="/" onClick={handleSignOut} className="header-item">
+                        <i className="header-item-icon fa fa-sign-out"></i>
+                        <p className="header-item-text">Sign Out</p>
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/login" className="header-item">
+                        <i className="header-item-icon fa fa-user-circle"></i>
+                        <p className="header-item-text">Sign In</p>
+                    </Link>
+                </div>
+            )}
         </header>
     )
 }
