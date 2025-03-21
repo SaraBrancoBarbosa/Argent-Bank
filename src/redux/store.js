8206177a98942ecf483da2b512c9b5892c68de78
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-03-21 16:00:22 +0100

    Redux store update: tokenSlice and profileSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
-import userSlice from "./userSlice"
+import tokenSlice from "./tokenSlice"
 import profileSlice from "./profileSlice"
-import categorySlice from "./categorySlice"
-import noteSlice from "./noteSlice"
 
-// Notre magasin de données Redux
 export const store = configureStore({
     reducer: {
-        user: userSlice.reducer,
+        token: tokenSlice.reducer,
         profile: profileSlice.reducer,
-        categories: categorySlice.reducer,
-        notes: noteSlice.reducer,
     },
 })
